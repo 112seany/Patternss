e42026d6caadb781d2107f356c2bd223b0a3b4c6
112seany <67510168+112seany@users.noreply.github.com>
2023-07-02 04:00:39 +0300

    pattern (#12)

--- src/main/java/structural/flyweight/Forest.java
+++ src/main/java/structural/flyweight/Forest.java
@@ -1,4 +1,4 @@
-package main.java.structural.Flyweight;
+package main.java.structural.flyweight;
 
 import javax.swing.*;
 import java.awt.*;
--- src/main/java/structural/flyweight/TreeType.java
+++ src/main/java/structural/flyweight/TreeType.java
@@ -1,4 +1,4 @@
-package main.java.structural.Flyweight;
+package main.java.structural.flyweight;
 
 import java.awt.*;
 
